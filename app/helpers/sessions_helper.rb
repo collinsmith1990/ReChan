969e14d9d645e3d358be7a0d5f8fdc5fb180db5a
CollinSmith <collinsmith1990@gmail.com>
2015-03-26 22:11:55 -0600

    Added sessions

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,25 @@
+module SessionsHelper
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def forget(user)
+    cookies.delete(:user_id)
+  end
+
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def current_user
+    user_id = session[:user_id]
+    @current_user ||= User.find_by(id: user_id)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,11 @@ class User < ActiveRecord::Base
                         uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }, allow_blank: true
+  before_save :down_case_user_name
 
+  private
+    
+    def down_case_user_name
+      self.user_name = user_name.downcase
+    end
 end
